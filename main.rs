79770105453264ec987848c0054596597186d89b
Naresh Kumar <31959509+naganareshkumar@users.noreply.github.com>
2026-01-14 17:33:04 +0000

    Update Rust code with Option pattern matching example

--- main.rs
+++ main.rs
@@ -1,4 +1,22 @@
-fun main() {
-    println!("Hello, World!")
-    
+fn main() {
+    println!("Hello, World!");
+    let m_n = Some(43);
+    if let Some(number) = m_n {
+        println!("{}", number);
+    } else {
+        println!("error");
+    }
+    let age = get_age();
+if let Some(num) = age {
+    println!("Age: {}", num);
+} else {
+    println!("No age found");  // This handles the None case
 }
+}
+
+
+// Might NOT have a value
+fn get_age() -> Option<i32> {
+    None  // or Some(25) - we don't know!
+}
+
